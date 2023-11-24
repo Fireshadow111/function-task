ebd5dfc26e363025bdc894a50053c5d25374700f
Fireshadow111 <muhammadrajah16@gmail.com>
2023-11-24 13:58:38 +0200

    ad

---
+++ index.js
@@ -0,0 +1,30 @@
+
+function greet(hello) {
+    hello = 'Hi';
+    console.log(hello);
+}
+greet()
+
+
+
+function muiltiply(number1, number2){
+    number1 = 2
+    number2 = 7
+    result = number1 * number2   
+     console.log(result)
+}
+
+
+ muiltiply()
+
+
+ 
+ function tableOfFive(table){
+
+    table = 2
+    result = 2 * 5 
+ 
+    console.log(result)
+ }
+
+ tableOfFive()
